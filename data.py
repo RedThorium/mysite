a48ff92a3663a24103699f0636810e6dc23b998c
sbm367 <sbm367@protonmail.com>
2018-12-08 16:02:49 -0500

    data processing and rendering

---
+++ data.py
@@ -0,0 +1,18 @@
+def MyData():
+    MyData = [
+        {
+            'id':1,
+            'body':'data body 1'
+        },
+        {
+            'id':2,
+            'body':'data body 2'
+
+        },
+        {
+            'id':3,
+            'body':'data body 2'
+        }
+
+    ]
+    return MyData
--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,11 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
+from data import MyData
 
 app = Flask(__name__)
+
+theData = MyData()
 
 @app.route('/')
 def hello_world():
@@ -15,4 +18,15 @@ def nan():
 
 @app.route('/home')
 def rend_home():
-    render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html', mydata = theData)
+
+if __name__ == '__main__':
+    app.run(debug=True)
